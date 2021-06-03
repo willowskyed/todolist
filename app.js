9192ee3cdff18a79637cde8b47dad6e127c098f0
Willow Daigle <willowenfedaque-daigle@Willows-MacBook-Pro.local>
2021-06-03 16:24:22 -0700

    functionality and basic styling

--- app.js
+++ app.js
@@ -1,15 +1,117 @@
 $(document).ready(function(){
+// update task list
+  var getAndDisplayAllTasks = function () {
   $.ajax({
     type: 'GET',
-    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=100',
     dataType: 'json',
     success: function (response, textStatus) {
+      $('#todo-list').empty();
       response.tasks.forEach(function (task) {
-        $('#todo-list').append('<p>' + task.content + '</p>');
-      })
+        $('#todo-list').append('<div class="row"><p class="col-xs-8 border">' + task.content + '</p><button class="delete btn" data-id="' + task.id + '"><i class="fas fa-minus-circle"></i></button><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '>');;
+      });
     },
     error: function (request, textStatus, errorMessage) {
       console.log(errorMessage);
     }
   });
+}
+
+// create post request
+  var createTask = function () {
+    $.ajax({
+      type: 'POST',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=100',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          content: $('#new-task-content').val()
+        }
+      }),
+      success: function (response, textStatus) {
+        $('#new-task-content').val('');
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+// link function to html
+  $('#create-task').on('submit', function (e) {
+    e.preventDefault();
+    createTask();
+  });
+
+  getAndDisplayAllTasks();
+
+// delete a task
+  var deleteTask = function (id) {
+    $.ajax({
+      type: 'DELETE',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '?api_key=100',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+// link delete function to delete buttons
+  $(document).on('click', '.delete', function () {
+    deleteTask($(this).data('id'));
+  });
+
+  $.ajax({
+    type: 'PUT',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/2844/mark_complete?api_key=100',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      console.log(response);
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+
+// mark task complete
+  var markAsComplete = function (id) {
+    $.ajax({
+      type: 'PUT',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_complete?api_key=100',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+// link mark complete function to checkbox
+  $(document).on('change', '.mark-complete', function () {
+    if (this.checked) {
+      markAsComplete($(this).data('id'));
+    } else {
+      markTaskActive($(this).data('id'));
+    }
+  });
+
+// mark a task as active
+  var markTaskActive = function (id) {
+    $.ajax({
+      type: 'PUT',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_active?api_key=100',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+}
+
 });
